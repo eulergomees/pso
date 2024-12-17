d6d75aecd4b88da8ffe94591751b152aac3af539
Euler <eulergomesrocha@gmail.com>
2024-12-17 15:09:32 -0300

    rastringin func

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import time
-
+import numpy as np
 
 maxit = 20
 w = 0.5
 c1 = 1.5
 c2 = 1.5
-numpart = 100
+particles = 100
 dimension = 2
+
+
+def rastringin(x):
+    n = len(x)
+    return 10 * n + sum([xi ** 2 - 10 * np.cos(2 * np.pi * xi) for xi in x])
